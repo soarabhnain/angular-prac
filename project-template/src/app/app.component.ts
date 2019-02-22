0c44c8388fca9379ed308c7b40ec5c07a4dc931c
Nain <335210@cognizant.com>
2019-02-22 12:34:08 +0530

    First commit

---
+++ project-template/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+  h3{
+    color:blue;
+  }
+  `]
+})
+export class AppComponent {
+}
