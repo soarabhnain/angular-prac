f9669cb0c5d6444e3d6f39a1b44b8cf2b80d0b25
Nain <335210@cognizant.com>
2019-02-25 19:54:06 +0530

    First commit

--- main-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ main-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is just a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'),
     new Recipe('A test recipe', 'this is just a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA')
   ];
 
